b0114eb635f035ce309c728565f7066b9db56fce
Sidhu <cherukusidhu@gmail.com>
2026-01-30 16:17:24 +0530

    final submission

--- src/com/revpasswordmanager/dao/UserDaoImpl.java
+++ src/com/revpasswordmanager/dao/UserDaoImpl.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoImpl implements IUserDao {
+public class   UserDaoImpl implements IUserDao {
 
     private static final Logger logger = LogManager.getLogger(UserDaoImpl.class);
     private Connection connection;
